a9cb366f15b90703bcd1c140191879d13687898c
Maick Souza <maick_a_s@msn.com>
2022-04-13 18:54:58 -0300

    feat(server): simple convertion image to color ASCII

---
+++ src/index.mjs
@@ -0,0 +1,23 @@
+import asciify from "asciify-image";
+
+var options = {
+  fit: "box",
+  width: 85,
+  height: 40,
+  color: true,
+};
+
+const url = "images/Hatsune01.jpg";
+
+async function asciifiyng(imageUrl) {
+  try {
+    const result = await asciify(`src/${imageUrl}`, options);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const image = await asciifiyng(url);
+
+console.log(image);
